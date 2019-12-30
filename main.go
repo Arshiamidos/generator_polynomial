821173431cb126cb7e6af658a0cd2a981bf441eb
ArashMidos <arshiamidos@gmail.com>
2019-12-30 22:40:35 +0330

    restructor

--- main.go
+++ main.go
@@ -3,13 +3,62 @@ import (
 	"fmt"
 )
 
+type GenPoly struct{
+	Poly map[int]int//Expo Coe
+}
+func (g *GenPoly) Init(p map[int]int) {
+	g.Poly=p
+}
+
+func (g *GenPoly) Sort() {
+
+	keys := []int{}
+	for k := range g.Poly {
+		keys = append(keys, k)
+	}
+
+	for i:=0;i<len(keys);i++{
+		for j:=i+1;j<len(keys);j++{
+			if keys[i] > keys[j] {
+				tmp:=keys[i]
+				keys[i]=keys[j]
+				keys[j]=tmp
+			}
+		}
+	}
+
+	p:=make(map[int]int)
+	for i:=0;i<len(keys);i++{
+		p[keys[i]]=g.Poly[keys[i]]
+	}
+	g.Poly=p
+
+}
+
+func (g *GenPoly) GetKeys() []int{
+
+	keys := []int{}
+	for k := range g.Poly {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (g *GenPoly) GetAll() map[int]int{
+	return g.Poly
+}
+
+
 type CoeExpo struct{
 	Coe  int;
 	Expo int;
 }
 
+
 func main(){
 	fmt.Println("ya");
+
+
 	CoeExpos := []CoeExpo{
 		CoeExpo{3,4},
 		CoeExpo{2,1},
@@ -29,16 +78,6 @@ func main(){
 			}
 		}
 	}
-	for i:=0;i<len(CoeExpos);i++{
-		if(i!=0){
-			if CoeExpos[i-1].Expo==CoeExpos[i].Expo {
-
-			}
-		}
-	}
-
-
-
 
 
 	fmt.Println(CoeExpos)
